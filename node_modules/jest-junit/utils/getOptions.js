35b4a7038403c6e2f3096177e51c653162925d62
yoezer33 <wangmoy172@gmail.com>
2026-05-12 14:09:50 +0600

    Fix package.json and add jest test

---
+++ node_modules/jest-junit/utils/getOptions.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+const constants = require('../constants/index');
+
+const { replaceRootDirInPath } = require('./replaceRootDirInPath');
+
+let uuidV1Promise;
+function loadUuidV1() {
+  if (!uuidV1Promise) {
+    uuidV1Promise = import('uuid').then((mod) => mod.v1);
+  }
+  return uuidV1Promise;
+}
+
+function getEnvOptions() {
+  const options = {};
+
+  for (let name in constants.ENVIRONMENT_CONFIG_MAP) {
+    if (process.env[name]) {
+      options[constants.ENVIRONMENT_CONFIG_MAP[name]] = process.env[name];
+    }
+  }
+
+  return options;
+}
+
+function getAppOptions(pathToResolve) {
+  let traversing = true;
+
+  // Get the root dir to detect when we reached the end to our search
+  const rootDir = path.parse(pathToResolve).root
+
+  // Find nearest package.json by traversing up directories until root
+  while(traversing) {
+    traversing = pathToResolve !== rootDir;
+
+    const pkgpath = path.join(pathToResolve, 'package.json');
+
+    if (fs.existsSync(pkgpath)) {
+      let options;
+
+      try {
+        options = (require(pkgpath) || {})['jest-junit'];
+      } catch (error) {
+        console.warn(`Unable to import package.json to get app Options : ${error}`)
+      }
+
+      if (Object.prototype.toString.call(options) !== '[object Object]') {
+        options = {};
+      }
+
+      return options;
+    } else {
+      pathToResolve = path.dirname(pathToResolve);
+    }
+  }
+
+  return {};
+}
+
+function replaceRootDirInOutput(rootDir, output) {
+  return rootDir !== null ? replaceRootDirInPath(rootDir, output) : output;
+}
+
+async function getUniqueOutputName(outputName) {
+  const v1 = await loadUuidV1();
+  const outputPrefix = outputName ? outputName : 'junit'
+  return `${outputPrefix}-${v1()}.xml`
+}
+
+module.exports = {
+  options: (reporterOptions = {}) => {
+    return Object.assign({}, constants.DEFAULT_OPTIONS, reporterOptions, getAppOptions(process.cwd()), getEnvOptions());
+  },
+  getAppOptions: getAppOptions,
+  getEnvOptions: getEnvOptions,
+  replaceRootDirInOutput: replaceRootDirInOutput,
+  getUniqueOutputName: getUniqueOutputName
+};
